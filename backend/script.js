9d908d86fc07d89eb12686ec357bf6fe639a3430
yuva charan <yuvacharanvps@gmail.com>
2026-06-28 20:31:09 +0530

    added 200 lines of code in core_logic

--- backend/script.js
+++ backend/script.js
@@ -195,3 +195,8 @@ function animateThreeJS() {
 }
 
 function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
