6d5d54d816bae5d691559c9604ad5ff7a58cb8f1
yuva charan <yuvacharanvps@gmail.com>
2026-06-28 20:24:16 +0530

    added 170 lines of code in core_logic

--- backend/script.js
+++ backend/script.js
@@ -158,3 +158,14 @@ function initThreeJS() {
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambientLight);
 
+    
+    // Resize Handler
+    window.addEventListener('resize', onWindowResize, false);
+
+    // Start Animation Loop
+    animateThreeJS();
+    
+    // Launch Sequence Timeline
+    triggerLaunchSequence();
+}
+
