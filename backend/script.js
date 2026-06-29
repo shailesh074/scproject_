9e3d5d4e541f20c037e8f6d0f13e985b11b1d9ef
yuva charan <yuvacharanvps@gmail.com>
2026-06-29 21:58:02 +0530

    added 5 lines of code in widget_engine

--- backend/script.js
+++ backend/script.js
@@ -1,1 +1,7 @@
 //THIS IS BACKEND;
+i   // --- 5. Mutual Fund SIP Projection (Compound Math) ---
+    const calcSIP = () => {
+        const p = +sel('sip-p').value;
+        const r_annual = +sel('sip-r').value;
+        const t_years = +sel('sip-t').value;
+        const res = sel('sip-res');
