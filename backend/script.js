c7f19670ab0846e4d952ead8a749b44a8881ad6b
yuva charan <yuvacharanvps@gmail.com>
2026-06-29 22:46:27 +0530

    added few lines of code in render_analytics

--- backend/script.js
+++ backend/script.js
@@ -1,3 +1,5 @@
 //THIS IS BACKEND;
 
     // Phase 4: Master Ledger Table (Transactions View)
+    const tb = sel('transactions-body'); tb.innerHTML = '';
+    if (appData.transactions.length === 0) {	
