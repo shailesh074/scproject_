17910e45a6cd3eb081735be6bd3ad8d302f53422
yuva charan <yuvacharanvps@gmail.com>
2026-06-29 22:47:21 +0530

    added few lines of code in render_analytics

--- backend/script.js
+++ backend/script.js
@@ -1,5 +1,7 @@
 //THIS IS BACKEND;
 
     // Phase 4: Master Ledger Table (Transactions View)
-    const tb = sel('transactions-body'); tb.innerHTML = '';
-    if (appData.transactions.length === 0) {	
+   i const tb = sel('transactions-body'); tb.innerHTML = '';
+    if (appData.transactions.length === 0) {
+        tb.innerHTML = '<tr><td colspan="5" class="text-center text-secondary p-8">No transaction data available for master ledger.</td></tr>';
+    } else {
