7a9d2acde38c34ad0695528b81639657401a0102
yuva charan <yuvacharanvps@gmail.com>
2026-06-29 11:50:58 +0530

    added 10 lines of code in widget_engine

--- backend/script.js
+++ backend/script.js
@@ -1,1 +1,10 @@
 //THIS IS BACKEND;
+ // 4b. Sidebar Routing Logic
+    document.querySelectorAll('.nav-links li').forEach(l => {
+        l.onclick = () => {
+            document.querySelectorAll('.nav-links li').forEach(x => x.classList.remove('active'));
+            l.classList.add('active');
+            
+            document.querySelectorAll('.app-view').forEach(v => {
+                v.style.opacity = '0'; 
+                setTimeout(() => v.classList.remove('active'), 250);
